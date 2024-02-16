0e3f5c48ccacc28788bf0ea648bdb8cb1ccf00a1
Ally Diah <159260474+AllyDiahDKN@users.noreply.github.com>
2024-02-16 13:37:33 +0300

    Update meta_tags.php

--- meta_tags.php
+++ meta_tags.php
@@ -18,6 +18,61 @@ switch ($currentPage) {
         $description = "Description for the About Us page.";
         $keywords = "about, company, information";
         break;
+
+        case "shop.php?brand_id=2":
+            $title = "Hp";
+            $description = "Description for the About Us page.";
+            $keywords = "about, company, information";
+            break;
+
+
+        case "shop.php?category_id=1":
+            $title = "all products";
+            $description = "Description for the About Us page.";
+            $keywords = "about, company, information";
+            break;
+
+            case "shop.php?category_id=2":
+                $title = "Laptops";
+                $description = "Description for the About Us page.";
+                $keywords = "about, company, information";
+                break;
+
+                    case "shop.php?category_id=3":
+                        $title = "Desktops";
+                        $description = "Description for the About Us page.";
+                        $keywords = "about, company, information";
+                        break;
+
+                            case "shop.php?category_id=4":
+                                $title = "printers";
+                                $description = "Description for the About Us page.";
+                                $keywords = "about, company, information";
+                                break;
+
+                                case "shop.php?category_id=5":
+                                    $title = "Toners & Catriges ";
+                                    $description = "Description for the About Us page.";
+                                    $keywords = "about, company, information";
+                                    break;
+
+                                    case "shop.php?category_id=6":
+                                        $title = "Softwares";
+                                        $description = "Description for the About Us page.";
+                                        $keywords = "about, company, information";
+                                        break;
+
+                                        case "shop.php?category_id=7":
+                                            $title = "Monitors ";
+                                            $description = "Description for the About Us page.";
+                                            $keywords = "about, company, information";
+                                            break;
+
+                                            case "shop.php?category_id=8":
+                                                $title = "Accessories";
+                                                $description = "Description for the About Us page.";
+                                                $keywords = "about, company, information";
+
     // Add more cases for other pages as needed
     default:
         $title = "Default Title";
